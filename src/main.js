996a3dcc59cc7c3874d1e16b3bc38a38f621a42e
ziwenying <s316108@gmail.com>
2022-07-24 17:12:04 +0800

    inital commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+// CSS
+import './assets/application.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
